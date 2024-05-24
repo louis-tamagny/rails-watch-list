8a992fd55dd891da1a69d221bedb3510fb396c55
Louis Tamagny <louis.tamagny@gmail.com>
2024-05-24 10:51:29 +0200

    before deployment
    add changes to styles
    add views for every actions

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
